4d3b4a5feec193894f5069b25e5d3114b3811e25
Jordan Kay <jordan@fleuronic.com>
2023-03-09 16:26:03 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
     name: "Ergo",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v16)
     ],
     products: [
         .library(
@@ -16,11 +16,11 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/square/workflow-swift", from: "1.0.0-rc.1"),
-        .package(url: "https://github.com/Fleuronic/Metric", .branch("main")),
+		.package(url: "https://github.com/Fleuronic/Metric", branch: "main"),
 		.package(url: "https://github.com/JohnSundell/Identity", from: "0.1.0"),
         .package(url: "https://github.com/DeclarativeHub/Layoutless", .upToNextMajor(from: "0.4.0")),
         .package(url: "https://github.com/DeclarativeHub/Bond", .upToNextMajor(from: "7.0.0")),
-		.package(url: "https://github.com/Fleuronic/Inject.git", .branch("main"))
+		.package(url: "https://github.com/Fleuronic/Inject.git", branch: "main")
     ],
     targets: [
         .target(
