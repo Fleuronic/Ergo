dede330eb2bc4650f95410ddf0b9aa2d917f4e1a
Jordan Kay <jordan@fleuronic.com>
2023-05-16 08:06:01 -0400

    Add submodules

--- Package.swift
+++ Package.swift
@@ -10,21 +10,10 @@ let package = Package(
         .library(
             name: "Ergo",
             targets: ["Ergo"]
-		),
-		.library(
-			name: "ErgoSwiftUI",
-			targets: ["ErgoSwiftUI"]
-		),
-		.library(
-			name: "ErgoUIKit",
-			targets: ["ErgoUIKit"]
 		)
     ],
     dependencies: [
         .package(url: "https://github.com/square/workflow-swift", from: "1.0.0-rc.1"),
-		.package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", branch: "swift-concurrency"),
-		.package(url: "https://github.com/ReactiveCocoa/ReactiveCocoa", from: "12.0.0"),
-        .package(url: "https://github.com/DeclarativeHub/Layoutless", .upToNextMajor(from: "0.4.0")),
 		.package(url: "https://github.com/gringoireDM/EnumKit", from: "1.1.0"),
 		.package(url: "https://github.com/Fleuronic/Inject.git", branch: "main")
     ],
@@ -36,23 +25,7 @@ let package = Package(
 				"Inject",
 				.product(name: "WorkflowUI", package: "workflow-swift"),
 				.product(name: "WorkflowReactiveSwift", package: "workflow-swift")
-			],
-			path: "Sources/Ergo"
-		),
-		.target(
-			name: "ErgoSwiftUI",
-			dependencies: ["Ergo"],
-			path: "Sources/ErgoSwiftUI"
-		),
-		.target(
-			name: "ErgoUIKit",
-			dependencies: [
-				"Ergo",
-				"Layoutless",
-				"ReactiveSwift",
-				"ReactiveCocoa"
-			],
-			path: "Sources/ErgoUIKit"
+			]
 		)
     ]
 )
