99f6bd824ffb6c701aedb87444caec59d8a23799
Jordan Kay <jkay@collegeboard.org>
2023-05-25 16:04:27 -0400

    Update Workflow dependency

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
 		)
     ],
     dependencies: [
-        .package(url: "https://github.com/square/workflow-swift", from: "1.0.0"),
+        .package(url: "https://github.com/Fleuronic/workflow-swift", branch: "main"),
 		.package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", branch: "swift-concurrency"),
 		.package(url: "https://github.com/gringoireDM/EnumKit", from: "1.1.0")
     ],
