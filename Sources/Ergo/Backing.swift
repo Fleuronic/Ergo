faa615ec39f006168a3ca5ad53ac2ba693a3b027
Jordan Kay <jordan@fleuronic.com>
2024-06-03 14:42:08 -0400

    Update backing

--- Sources/Ergo/Backing.swift
+++ Sources/Ergo/Backing.swift
@@ -8,12 +8,4 @@ public protocol ScreenBacked {
 	associatedtype Screen: WorkflowUI.Screen
 }
 
-#elseif canImport(AppKit)
-
-import protocol WorkflowMenuUI.Pane
-
-public protocol PaneBacked {
-	associatedtype Pane: WorkflowMenuUI.Pane
-}
-
 #endif
